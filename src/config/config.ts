a02130551fec512166e66c28e7ded6670d71ba46
Edward <senlion51@gmail.com>
2022-10-15 02:45:03 +0300

    Setup CORS

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,6 @@
 export const serverPort = "https://rom-api.onrender.com";
-const originPort = "http://localhost:3000";
+const originPort =
+  "https://senlion5.github.io/rings-of-might-rooms" || "http://localhost:3000";
 export const extraHeaders = {
   "Access-Control-Allow-Origin": originPort,
 };
