62fbb2a5d82e98bc2d39ec6656502b64d7a83a26
Edward <senlion51@gmail.com>
2022-10-16 18:43:25 +0300

    Changed to render server

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-export const serverPort = "https://rom-api-production.up.railway.app/";
+export const serverPort = "https://rom-api.onrender.com";
 const originPort = "https://senlion5.github.io/rings-of-might-rooms";
 export const extraHeaders = {
   "Access-Control-Allow-Origin": originPort,
